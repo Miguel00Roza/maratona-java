ebce825ddcbbdfebb7bee5d4388f1922b0ed01f0
Miguel00Roza <miguelroza13@gmail.com>
2026-02-15 19:42:45 -0300

    Criação de pacote e novamente Hello World

---
+++ src/io/github/miguel00roza/maratonajava/introducao/HelloWorld.java
@@ -0,0 +1,8 @@
+package io.github.miguel00roza.maratonajava.introducao;
+// No java, quando criamos um pacote, obrigatoriamente a primeira linha deve ser o seu pacote
+
+public class HelloWorld {
+    public static void main(String [] args) {
+        System.out.println("Hello, World!");
+    }
+}
